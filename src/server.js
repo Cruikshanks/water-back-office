6ddeefdb336fb568c5c98208cfcc1044606566a9
Rebecca Ransome <beckyrose200@aol.com>
2025-03-31 11:58:12 +0100

    Initial commit for water-back-office with View Licence endpoint

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const Hapi = require('@hapi/hapi')
+const { getLicenceDetails } = require('water-engine')
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3002,
+    host: 'localhost'
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/licence/{licenceId}',
+    handler: (request, h) => {
+      const { licenceId } = request.params
+      return getLicenceDetails(licenceId)
+    }
+  })
+
+  await server.start()
+  console.log('Back-office server running on %s', server.info.uri)
+}
+
+init()
