6fc9ddb79d7e254ac27845f783e026a13b90931d
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-07-20 14:29:23 +0100

    Add shared root routes from the engine
    So we want to confirm the session cookie handling, which means we need to start looking at adding a view.
    
    A nice starter is to look at seeing if we can share certain routes and endpoints in the apps, and there is none more simple than the `/status` end point.
    
    We add routes and controllers to the engine for the first time, and export 'routes' in the package.json.
    
    We then simply import `RootRoutes` from the engine in both apps `RouterPlugin`, and add them to the list of routes.
    
    Job done!

--- src/plugins/router.plugin.js
+++ src/plugins/router.plugin.js
@@ -1,7 +1,9 @@
+import RootRoutes from 'water-engine/routes/root.routes.js'
+
 import BillRunRoutes from '../routes/bill-runs.routes.js'
 import LicenceRoutes from '../routes/licences.routes.js'
 
-const routes = [...BillRunRoutes, ...LicenceRoutes]
+const routes = [...RootRoutes, ...BillRunRoutes, ...LicenceRoutes]
 
 export default {
   name: 'router',
