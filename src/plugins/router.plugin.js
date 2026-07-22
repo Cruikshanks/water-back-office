c2238218aa633b3a0f1f3e011cedaa73434f177c
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-07-22 17:43:01 +0100

    Move shared routes into engine
    We avoided setting routes in the engine to begin with as we worked our way through this new setup.
    
    But having sorted the view-engine, things like the root and assets routes would make better sense to be registered on the base-server instance to avoid the apps having to do it. They can then just focus on their own routes.

--- src/plugins/router.plugin.js
+++ src/plugins/router.plugin.js
@@ -1,10 +1,7 @@
-import AssetRoutes from 'water-engine/routes/assets.routes.js'
-import RootRoutes from 'water-engine/routes/root.routes.js'
-
 import BillRunRoutes from '../routes/bill-runs.routes.js'
 import LicenceRoutes from '../routes/licences.routes.js'
 
-const routes = [...RootRoutes, ...AssetRoutes, ...BillRunRoutes, ...LicenceRoutes]
+const routes = [...BillRunRoutes, ...LicenceRoutes]
 
 export default {
   name: 'router',
